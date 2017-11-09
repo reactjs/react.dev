1ab70d014d3031b122ee13f6f052511ca29aa7bf
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-11-09 08:49:13 +0000

    Updated gatsby-config to reflect minor plug-in changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,10 @@ module.exports = {
             options: {
               defaultText: 'Try it on CodePen',
               directory: `${__dirname}/examples/`,
+              externals: [
+                `//unpkg.com/react/umd/react.development.js`,
+                `//unpkg.com/react-dom/umd/react-dom.development.js`,
+              ],
               redirectTemplate: `${__dirname}/src/templates/codepen-example.js`,
               target: '_blank',
             },

d426e71e9afd1534aa441cf74b4f6d9d58f65390
Clement Hoang <clemmy@fb.com>
2017-11-10 16:04:07 +0000

    Update script to filter locales with certain progress thresholds and symlink them

--- crowdin/config.js
+++ crowdin/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   key: process.env.CROWDIN_API_KEY,
   url: 'https://api.crowdin.com/api/project/react',
+  translation_threshold: 30,
 };
--- crowdin/download.js
+++ crowdin/download.js
@@ -1,6 +1,23 @@
-var Crowdin = require('crowdin');
+var Crowdin = require('crowdin-node');
 var config = require('./config');
+var path = require('path');
 
 var crowdin = new Crowdin({ apiKey: config.key, endpointUrl: config.url });
+process.chdir(path.resolve(__dirname, 'translations'));
 
-crowdin.downloadToPath('./translations');
+crowdin.downloadToPath(path.resolve(__dirname, '__translations'));
+
+crowdin.getTranslationStatus().then(locales => {
+  const usableLocales = locales.filter(locale => locale.translated_progress > config.translation_threshold);
+
+  usableLocales.forEach(locale => {
+    createSymLink(locale.code);
+  });
+});
+
+function createSymLink(localeName) {
+  fs.symlink('../../content/' + localeName, localeName, (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}

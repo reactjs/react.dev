0ac23878c62aeb1cb3c4539ac4d61dccda6452af
Clement Hoang <clemmy@fb.com>
2017-11-11 23:47:50 +0000

    Update download script to symlink downloaded translations

--- crowdin/download.js
+++ crowdin/download.js
@@ -1,23 +1,100 @@
-var Crowdin = require('crowdin-node');
-var config = require('./config');
-var path = require('path');
+const Crowdin = require('crowdin-node');
+const config = require('./config');
+const path = require('path');
+const {symlink, lstatSync, readdirSync} = require('fs');
 
-var crowdin = new Crowdin({ apiKey: config.key, endpointUrl: config.url });
-process.chdir(path.resolve(__dirname, 'translations'));
+const SYMLINKED_TRANSLATIONS_PATH = path.resolve(__dirname, 'translations');
+const DOWNLOADED_TRANSLATIONS_PATH = path.resolve(__dirname, '__translations');
+const DOWNLOADED_TRANSLATIONS_DOCS_PATH = path.resolve(
+  __dirname,
+  '__translations',
+  'docs',
+);
 
-crowdin.downloadToPath(path.resolve(__dirname, '__translations'));
+function main() {
+  const crowdin = new Crowdin({apiKey: config.key, endpointUrl: config.url});
+  process.chdir(SYMLINKED_TRANSLATIONS_PATH);
 
-crowdin.getTranslationStatus().then(locales => {
-  const usableLocales = locales.filter(locale => locale.translated_progress > config.translation_threshold);
+  crowdin
+    .downloadToPath(DOWNLOADED_TRANSLATIONS_PATH)
+    .then(() => {
+      return crowdin.getTranslationStatus();
+    })
+    .then(locales => {
+      const usableLocales = locales
+        .filter(
+          locale => locale.translated_progress > config.translation_threshold,
+        )
+        .map(local => local.code);
 
-  usableLocales.forEach(locale => {
-    createSymLink(locale.code);
-  });
-});
+      const localeDirectories = getDirectories(
+        DOWNLOADED_TRANSLATIONS_DOCS_PATH,
+      );
 
-function createSymLink(localeName) {
-  fs.symlink('../../content/' + localeName, localeName, (err) => {
-    console.error(err);
-    process.exit(1);
+      const localeToFolderMap = createLocaleToFolderMap(localeDirectories);
+
+      usableLocales.forEach(locale => {
+        createSymLink(localeToFolderMap.get(locale));
+      });
+    });
+}
+
+// Creates a relative symlink from a downloaded translation in the current working directory
+// Note that the current working directory of this node process should be where the symlink is created
+// or else the relative paths would be incorrect
+function createSymLink(folder) {
+  symlink(`../__translations/docs/${folder}`, folder, err => {
+    if (!err) {
+      console.log(`Created symlink for ${folder}.`);
+      return;
+    }
+
+    if (err.code === 'EEXIST') {
+      console.log(
+        `Skipped creating symlink for ${folder}. A symlink already exists.`,
+      );
+    } else {
+      console.error(err);
+      process.exit(1);
+    }
   });
 }
+
+// When we run getTranslationStatus(), it gives us 2-ALPHA locale codes unless necessary
+// However, the folder structure of downloaded translations always has 4-ALPHA locale codes
+// This function creates a map from a locale code to its corresponding folder name
+function createLocaleToFolderMap(directories) {
+  const twoAlphaLocale = locale => locale.substring(0, 2);
+  const localeToFolders = new Map();
+  const localeToFolder = new Map();
+
+  for (let locale of directories) {
+    localeToFolders.set(
+      twoAlphaLocale(locale),
+      localeToFolders.has(twoAlphaLocale(locale))
+        ? localeToFolders.get(twoAlphaLocale(locale)).concat(locale)
+        : [locale],
+    );
+  }
+
+  localeToFolders.forEach((folders, locale) => {
+    if (folders.length === 1) {
+      localeToFolder.set(locale, folders[0]);
+    } else {
+      for (let folder of folders) {
+        localeToFolder.set(folder, folder);
+      }
+    }
+  });
+
+  return localeToFolder;
+}
+
+function getDirectories(source) {
+  return readdirSync(source).filter(
+    name =>
+      lstatSync(path.join(source, name)).isDirectory() && name !== '_data',
+  );
+}
+
+main();

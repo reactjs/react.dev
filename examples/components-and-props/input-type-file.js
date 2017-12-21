b76ec15e08562ebd1add26f241bf5b7c55e1045d
Vishal Raj <vishalraj1802@gmail.com>
2017-12-21 20:00:26 +0530

    gatsby-remark-code-repls-modification

---
+++ examples/components-and-props/input-type-file.js
@@ -0,0 +1,26 @@
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+    alert(`Selected file - ${this.fileInput.files[0].name}`);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Upload file:
+          <input type='file' ref={input => {this.fileInput = input}} />
+        </label>
+        <br/>
+        <button type='submit'>Submit</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<FileInput />, document.getElementById('root'));

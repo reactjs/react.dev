55650fc12c6902826efcdccba076c45decb884c4
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-03-15 08:48:01 -0700

    Added explicit null value in state initializer

--- examples/update-on-async-rendering/updating-state-from-props-after.js
+++ examples/update-on-async-rendering/updating-state-from-props-after.js
@@ -2,18 +2,19 @@
 class ExampleComponent extends React.Component {
   // Initialize state in constructor,
   // Or with a property initializer.
-  // highlight-range{1-3}
+  // highlight-range{1-4}
   state = {
     isScrollingDown: false,
+    lastRow: null,
   };
   // highlight-line
   // highlight-range{1-8}
   static getDerivedStateFromProps(nextProps, prevState) {
     if (nextProps.currentRow !== prevState.lastRow) {
       return {
-        lastRow: nextProps.currentRow,
         isScrollingDown:
           nextProps.currentRow > prevState.lastRow,
+        lastRow: nextProps.currentRow,
       };
     }
 

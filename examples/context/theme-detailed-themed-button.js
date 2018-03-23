69198013e1eff940aba3649f7197737b0de8d8a2
Alex Krolick <alexander.krolick@gmail.com>
2018-03-23 11:46:47 -0700

    Use functional component

--- examples/context/theme-detailed-themed-button.js
+++ examples/context/theme-detailed-themed-button.js
@@ -1,19 +1,17 @@
 import {ThemeContext} from './theme-context';
 
-class ThemedButton extends React.Component {
-  // highlight-range{3-10}
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {theme => (
-          <button
-            {...this.props}
-            style={{backgroundColor: theme.background}}
-          />
-        )}
-      </ThemeContext.Consumer>
-    );
-  }
+function ThemedButton(props) {
+  // highlight-range{2-9}
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <button
+          {this.props}
+          style={{backgroundColor: theme.background}}
+        />
+      )}
+    </ThemeContext.Consumer>
+  );
 }
 
 export default ThemedButton;

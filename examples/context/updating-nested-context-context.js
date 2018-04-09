8ffa0f9bfb6e2689dac85bbfffe124385d1c5f4e
tjallingt <tjallingt@gmail.com>
2018-04-09 18:50:07 +0200

    noop function as default value to createContext

--- examples/context/updating-nested-context-context.js
+++ examples/context/updating-nested-context-context.js
@@ -1,10 +1,8 @@
 // highlight-range{1-2}
 // Make sure the shape of the default value passed to
 // createContext matches the shape that the consumers expect!
-// highlight-range{2-4}
+// highlight-range{2-3}
 export const ThemeContext = React.createContext({
   theme: themes.dark,
-  // implement default behavior...
-  toggleTheme: () =>
-    console.error('default toggleTheme called'),
+  toggleTheme: () => {},
 });

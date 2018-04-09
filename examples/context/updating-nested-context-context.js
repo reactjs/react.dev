6521a654cbb7e39fe23863579be86ef36dc8283f
tjallingt <tjallingt@gmail.com>
2018-04-09 20:12:02 +0200

    Added updating nested context default value example (#780)
    * added context default value example
    
    * Implement suggested change
    
    * noop function as default value to createContext

---
+++ examples/context/updating-nested-context-context.js
@@ -0,0 +1,8 @@
+// highlight-range{1-2}
+// Make sure the shape of the default value passed to
+// createContext matches the shape that the consumers expect!
+// highlight-range{2-3}
+export const ThemeContext = React.createContext({
+  theme: themes.dark,
+  toggleTheme: () => {},
+});

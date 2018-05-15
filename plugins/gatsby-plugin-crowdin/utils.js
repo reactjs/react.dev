3737457e8bf8af7feb3e097c33ec56d4e75c0c1c
Brian Vaughn <bvaughn@fb.com>
2018-05-15 07:46:38 -0700

    Made language code parsing more robust

--- plugins/gatsby-plugin-crowdin/utils.js
+++ plugins/gatsby-plugin-crowdin/utils.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// Parses language code (eg en, zh-CH) from a path (eg en/path/to/file.js)
+// Parses language code (e.g. en, zh-CH, fil-PH) from a path (eg en/path/to/file.js)
 // Returns null if path doesn't contain a language code.
 exports.getLanguageCodeFromPath = path => {
-  const match = path.match(/^([a-z]{2}|[a-z]{2}-[A-Z]+)\//);
+  const match = path.match(/^([a-z]{2}|[a-z]{2,}-[A-Z]+)\//);
 
   return match ? match[1] : null;
 };

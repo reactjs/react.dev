a5db230efd102e4d100a44c110ad8870c7ffea15
Alex Krolick <alexander.krolick@gmail.com>
2018-07-10 23:58:08 -0700

    Revert "remove sharp plugin"
    This reverts commit bbbb13d484f1308d3ff4bed258c926c24fddea6a.

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,6 +90,8 @@ module.exports = {
         ],
       },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {

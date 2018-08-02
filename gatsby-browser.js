563305e7f7b3fe7b0a4756415f6982507f3113e4
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-08-02 22:24:55 +0200

    Add fb comment and convert to cjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,17 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
 // Import global styles
-import './src/prism-styles';
-import 'glamor/reset';
-import './src/css/reset.css';
-import './src/css/algolia.css';
+require('./src/prism-styles');
+require('glamor/reset');
+require('./src/css/reset.css');
+require('./src/css/algolia.css');
 
 // A stub function is needed because gatsby won't load this file otherwise
 // (https://github.com/gatsbyjs/gatsby/issues/6759)
-export const onClientEntry = () => {};
+exports.onClientEntry = () => {};

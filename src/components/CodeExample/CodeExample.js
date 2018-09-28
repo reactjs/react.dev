57cb7abde1aa304b3cba30480cdd8036790d3d0a
Dustin Schau <dustinschau@gmail.com>
2018-09-28 14:32:36 -0500

    chore: remove outer wrapper div

--- src/components/CodeExample/CodeExample.js
+++ src/components/CodeExample/CodeExample.js
@@ -13,56 +13,45 @@ class CodeExample extends Component {
           '&:first-child': {
             marginTop: 0,
           },
-
           [media.greaterThan('xlarge')]: {
+            display: 'flex',
+            flexDirection: 'row',
             marginTop: 80,
           },
+
+          [media.lessThan('large')]: {
+            display: 'block',
+          },
         }}>
-        <div
-          css={{
-            [media.greaterThan('xlarge')]: {
-              display: 'flex',
-              flexDirection: 'row',
-            },
+        {children && (
+          <div
+            css={{
+              flex: '0 0 33%',
 
-            [media.lessThan('large')]: {
-              display: 'block',
-            },
-          }}>
-          {children && (
-            <div
-              css={{
-                flex: '0 0 33%',
+              [media.lessThan('xlarge')]: {
+                marginBottom: 20,
+              },
 
-                [media.lessThan('xlarge')]: {
-                  marginBottom: 20,
+              '& h3': {
+                color: colors.dark,
+                maxWidth: '11em',
+                paddingTop: 0,
+              },
+
+              '& p': {
+                marginTop: 15,
+                marginRight: 40,
+                lineHeight: 1.7,
+
+                [media.greaterThan('xlarge')]: {
+                  marginTop: 25,
                 },
-
-                '& h3': {
-                  color: colors.dark,
-                  maxWidth: '11em',
-                  paddingTop: 0,
-                },
-
-                '& p': {
-                  marginTop: 15,
-                  marginRight: 40,
-                  lineHeight: 1.7,
-
-                  [media.greaterThan('xlarge')]: {
-                    marginTop: 25,
-                  },
-                },
-              }}>
-              {children}
-            </div>
-          )}
-          {loaded ? (
-            <CodeEditor code={code} />
-          ) : (
-            <h4>Loading code example...</h4>
-          )}
-        </div>
+              },
+            }}>
+            {children}
+          </div>
+        )}
+        {loaded ? <CodeEditor code={code} /> : <h4>Loading code example...</h4>}
       </div>
     );
   }

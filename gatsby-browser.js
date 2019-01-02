8127896a946a6b5844ad20946189ecb2a9fac1fd
Daniel Husar <dano.husar@gmail.com>
2019-01-02 11:24:57 -0800

    Expose react as global (#1501)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,11 +6,18 @@
 
 'use strict';
 
+const React = require('react');
+const ReactDOM = require('react-dom');
+
 // Import global styles
 require('normalize.css');
 require('./src/css/reset.css');
 require('./src/prism-styles');
 require('./src/css/algolia.css');
+
+// Expose React and ReactDOM as globals for console playground
+window.React = React;
+window.ReactDOM = ReactDOM;
 
 // A stub function is needed because gatsby won't load this file otherwise
 // (https://github.com/gatsbyjs/gatsby/issues/6759)

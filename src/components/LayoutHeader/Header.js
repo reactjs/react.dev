88ea9c8b97246577c7dc60c0b515b226f279b3b8
Brian Vaughn <bvaughn@fb.com>
2019-02-17 14:36:09 -0800

    Added language icon. Show search icon in mobile.

--- src/components/LayoutHeader/Header.js
+++ src/components/LayoutHeader/Header.js
@@ -139,12 +139,16 @@ const Header = ({location}: {location: Location}) => (
 
         <div
           css={{
-            [media.lessThan('medium')]: {
-              display: 'none',
-            },
-            [media.greaterThan('large')]: {
-              width: 'calc(100% / 4)',
-            },
+            display: 'flex',
+            alignItems: 'center',
+            width: 'auto',
+
+            //[media.lessThan('medium')]: {
+            //width: 'auto',
+            //},
+            //[media.greaterThan('large')]: {
+            //width: 'calc(100% / 4)',
+            //},
           }}>
           <Link
             css={{
@@ -161,12 +165,18 @@ const Header = ({location}: {location: Location}) => (
                 backgroundColor: colors.lighter,
                 borderRadius: 15,
               },
+
+              [media.lessThan('medium')]: {
+                display: 'none',
+              },
             }}
             to="/versions">
             v{version}
           </Link>
           <Link
             css={{
+              display: 'flex',
+              alignItems: 'center',
               padding: '5px 10px',
               whiteSpace: 'nowrap',
               ...fonts.small,
@@ -182,7 +192,15 @@ const Header = ({location}: {location: Location}) => (
               },
             }}
             to="/languages">
-            Languages
+            <LanguagesIcon />{' '}
+            <span
+              css={{
+                [media.lessThan('medium')]: {
+                  display: 'none',
+                },
+              }}>
+              Languages
+            </span>
           </Link>
           <a
             css={{
@@ -221,6 +239,26 @@ const Header = ({location}: {location: Location}) => (
       </div>
     </Container>
   </header>
+);
+
+const LanguagesIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    css={{marginRight: '0.5rem'}}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+      css={{fill: 'currentColor'}}
+      d="
+        M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5
+        7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09
+        5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62
+        7l1.62-4.33L19.12 17h-3.24z
+      "
+    />
+  </svg>
 );
 
 export default Header;

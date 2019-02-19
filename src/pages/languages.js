14381e32dc876a7e24e2ee318f3f8f0fec0e02b2
Brian Vaughn <bvaughn@fb.com>
2019-02-19 14:26:35 -0800

    Split languages into 3 categories based on progress: complete, partially translated, and needs contributors

--- src/pages/languages.js
+++ src/pages/languages.js
@@ -15,12 +15,27 @@ import {media, sharedStyles} from 'theme';
 // $FlowFixMe This is a valid path
 import languages from '../../content/languages.yml';
 
-const completedLanguages = languages
-  .filter(language => language.is_completed)
-  .sort((a, b) => a.code.localeCompare(b.code));
-const inProgressLanguages = languages
-  .filter(language => !language.is_completed)
-  .sort((a, b) => a.code.localeCompare(b.code));
+// Status enums indicate what percentage of "core" content has been translated:
+// 0: Incomplete (0–49%)
+// 1: Partially complete (50–94%)
+// 2: Complete (95–100%)
+const {complete, incomplete, partial} = languages.reduce(
+  (reduced, language) => {
+    switch (language.status) {
+      case 0:
+        reduced.incomplete.push(language);
+        break;
+      case 1:
+        reduced.partial.push(language);
+        break;
+      case 2:
+        reduced.complete.push(language);
+        break;
+    }
+    return reduced;
+  },
+  {complete: [], incomplete: [], partial: []},
+);
 
 type Props = {
   location: Location,
@@ -39,39 +54,13 @@ const Languages = ({location}: Props) => (
               The React documentation is available in the following languages:
             </p>
 
-            <ul
-              css={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                marginLeft: -20,
-              }}>
-              {completedLanguages.map(language => (
-                <Language
-                  key={language.code}
-                  code={language.code}
-                  name={language.name}
-                  translatedName={language.translated_name}
-                />
-              ))}
-            </ul>
+            <LanguagesGrid languages={complete} />
 
-            <h2>In Progress</h2>
+            <h2>Partially Translated</h2>
+            <LanguagesGrid languages={partial} />
 
-            <ul
-              css={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                marginLeft: -20,
-              }}>
-              {inProgressLanguages.map(language => (
-                <Language
-                  key={language.code}
-                  code={language.code}
-                  name={language.name}
-                  translatedName={language.translated_name}
-                />
-              ))}
-            </ul>
+            <h2>Needs Contributors</h2>
+            <LanguagesGrid languages={incomplete} />
 
             <p>
               Don't see your language above?{' '}
@@ -90,53 +79,79 @@ const Languages = ({location}: Props) => (
   </Layout>
 );
 
-const Language = ({code, name, translatedName}) => (
-  <li
+const LanguagesGrid = ({languages}) => (
+  <ul
     css={{
-      paddingLeft: 20,
-      paddingTop: 20,
-      borderTop: '1px dotted #ececec',
-      paddingBottom: 20,
-      width: '100%',
-      listStyle: 'none',
-
-      [media.size('small')]: {
-        width: '50%',
-      },
-
-      [media.size('medium')]: {
-        width: '33.33%',
-      },
-
-      [media.greaterThan('large')]: {
-        width: '25%',
-      },
-    }}
-    key={code}>
-    <div css={{}}>{name}</div>
-    <div
-      css={{
-        fontSize: 22,
-        fontWeight: 'bold',
-        marginBottom: 8,
-        marginTop: 8,
-      }}>
-      <a href={`https://${code}.reactjs.org/`} rel="nofollow">
-        {translatedName}
-      </a>
-    </div>
-    <div css={{marginTop: 10}}>
-      <a
-        css={{
-          fontSize: 12,
-        }}
-        href={`https://github.com/reactjs/${code}.reactjs.org/`}
-        target="_blank"
-        rel="noopener">
-        Contribute
-      </a>
-    </div>
-  </li>
+      display: 'flex',
+      flexWrap: 'wrap',
+      marginLeft: -20,
+    }}>
+    {languages.map(({code, name, status, translated_name}) => (
+      <Language
+        key={code}
+        code={code}
+        name={name}
+        status={status}
+        translatedName={translated_name}
+      />
+    ))}
+  </ul>
 );
+
+const Language = ({code, name, status, translatedName}) => {
+  const prefix = code ? `${code}.` : '';
+
+  return (
+    <li
+      css={{
+        paddingLeft: 20,
+        paddingTop: 20,
+        borderTop: '1px dotted #ececec',
+        paddingBottom: 20,
+        width: '100%',
+        listStyle: 'none',
+
+        [media.size('small')]: {
+          width: '50%',
+        },
+
+        [media.size('medium')]: {
+          width: '33.33%',
+        },
+
+        [media.greaterThan('large')]: {
+          width: '25%',
+        },
+      }}
+      key={code}>
+      <div css={{}}>{name}</div>
+      <div
+        css={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginBottom: 8,
+          marginTop: 8,
+        }}>
+        {status === 0 && translatedName}
+        {status > 0 && (
+          <a href={`https://${prefix}reactjs.org/`} rel="nofollow">
+            {translatedName}
+          </a>
+        )}
+      </div>
+      <div css={{marginTop: 10}}>
+        <a
+          css={{
+            fontSize: 12,
+          }}
+          href={`https://github.com/reactjs/${prefix}reactjs.org/`}
+          target="_blank"
+          rel="noopener">
+          Contribute
+        </a>
+      </div>
+    </li>
+  );
+};
 
 export default Languages;

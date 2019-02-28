546994e2549a687c867eea0ff2ad1327390b40a3
Maxime Thirouin <git@moox.io>
2019-02-28 07:47:10 +0100

    Better favicon
    The goal of this commit is to offer a better favicon (& friends) to this site.
    This will looks better in all browsers/OS when added to favorites etc.
    
    Currently it looks like this in Safari
    
    <img width="311" alt="image" src="https://user-images.githubusercontent.com/157534/53546314-4a243f00-3b2c-11e9-9abe-6fb83fe324bd.png">
    
    With this change, it will looks better
    
    <img width="119" alt="image" src="https://user-images.githubusercontent.com/157534/53546498-cd459500-3b2c-11e9-9892-664706cd6caa.png">
    
    I used http://realfavicongenerator.net to generate this versions from a proper svg using color codes from the site.

--- src/html.js
+++ src/html.js
@@ -18,7 +18,28 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#20232a" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#20232a" />
+          <meta name="theme-color" content="#ffffff" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>

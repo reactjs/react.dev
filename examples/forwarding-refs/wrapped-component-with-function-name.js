e47d3a6bdfc81ba8546b53693f7f31a5aefa3a8f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-03-15 18:10:47 +0100

    format code

--- examples/forwarding-refs/wrapped-component-with-function-name.js
+++ examples/forwarding-refs/wrapped-component-with-function-name.js
@@ -1,9 +1,10 @@
-const FancyButton = React.forwardRef(
-  function Button(props, ref) {
-    return (
-      <button ref={ref} className="FancyButton">
-        {props.children}
-      </button>
-    );
-  }
-);
+const FancyButton = React.forwardRef(function Button(
+  props,
+  ref
+) {
+  return (
+    <button ref={ref} className="FancyButton">
+      {props.children}
+    </button>
+  );
+});

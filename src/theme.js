f6ce4f537586230bb63227e26a2c8d30122e0b3f
Juan Camilo Giraldo Ch <camilogiraldo91@gmail.com>
2019-04-03 13:18:30 -0500

    Fix 1640: Formatting for code blocks nested in blockquote

--- src/theme.js
+++ src/theme.js
@@ -405,6 +405,10 @@ const sharedStyles = {
           marginTop: 0,
         },
       },
+
+      '& .gatsby-highlight': {
+        marginLeft: 0,
+      },
     },
 
     '& .gatsby-highlight + blockquote': {

9e7b782b9960cbcac9690e01331ad80edc63f511
Muhammed Abidh <abidh.k@appsintegra.com>
2019-06-16 22:02:40 +0400

    * CodeExample component converted to stateless function

--- src/components/CodeExample/CodeExample.js
+++ src/components/CodeExample/CodeExample.js
@@ -1,71 +1,68 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
 import {colors, media} from 'theme';
 import CodeEditor from '../CodeEditor/CodeEditor';
 
-class CodeExample extends Component {
-  render() {
-    const {children, code, id, containerNodeID, loaded} = this.props;
-    return (
-      <div
-        id={id}
-        css={{
-          marginTop: 40,
 
-          '&:first-child': {
-            marginTop: 0,
-          },
+const CodeExample = ({children, code, id, containerNodeID, loaded}) => {
+  return (
+    <div
+      id={id}
+      css={{
+        marginTop: 40,
 
-          '& .react-live': {
-            width: '100%',
-          },
+        '&:first-child': {
+          marginTop: 0,
+        },
 
-          [media.greaterThan('xlarge')]: {
-            display: 'flex',
-            flexDirection: 'row',
-            marginTop: 80,
-          },
+        '& .react-live': {
+          width: '100%',
+        },
 
-          [media.lessThan('large')]: {
-            display: 'block',
-          },
-        }}>
-        {children && (
-          <div
-            css={{
-              flex: '0 0 33%',
-              [media.lessThan('xlarge')]: {
-                marginBottom: 20,
+        [media.greaterThan('xlarge')]: {
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: 80,
+        },
+
+        [media.lessThan('large')]: {
+          display: 'block',
+        },
+      }}>
+      {children && (
+        <div
+          css={{
+            flex: '0 0 33%',
+            [media.lessThan('xlarge')]: {
+              marginBottom: 20,
+            },
+
+            '& h3': {
+              color: colors.dark,
+              maxWidth: '11em',
+              paddingTop: 0,
+            },
+
+            '& p': {
+              marginTop: 15,
+              marginRight: 40,
+              lineHeight: 1.7,
+
+              [media.greaterThan('xlarge')]: {
+                marginTop: 25,
               },
-
-              '& h3': {
-                color: colors.dark,
-                maxWidth: '11em',
-                paddingTop: 0,
-              },
-
-              '& p': {
-                marginTop: 15,
-                marginRight: 40,
-                lineHeight: 1.7,
-
-                [media.greaterThan('xlarge')]: {
-                  marginTop: 25,
-                },
-              },
-            }}>
-            {children}
-          </div>
-        )}
-        {loaded ? (
-          <CodeEditor code={code} containerNodeID={containerNodeID} />
-        ) : (
-          <h4>Loading code example...</h4>
-        )}
-      </div>
-    );
-  }
+            },
+          }}>
+          {children}
+        </div>
+      )}
+      {loaded ? (
+        <CodeEditor code={code} containerNodeID={containerNodeID} />
+      ) : (
+        <h4>Loading code example...</h4>
+      )}
+    </div>
+  );
 }
 
 CodeExample.propTypes = {

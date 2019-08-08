cf6a12e097ddddd2327157915f7a4677bdac5b29
Brian Vaughn <bvaughn@fb.com>
2019-08-08 14:25:23 -0700

    Update website to v16.9

--- src/site-constants.js
+++ src/site-constants.js
@@ -8,7 +8,7 @@
 // NOTE: We can't just use `location.toString()` because when we are rendering
 // the SSR part in node.js we won't have a proper location.
 const urlRoot = 'https://reactjs.org';
-const version = '16.8.6';
+const version = '16.9.0';
 const babelURL = 'https://unpkg.com/babel-standalone@6.26.0/babel.min.js';
 
 export {babelURL, urlRoot, version};

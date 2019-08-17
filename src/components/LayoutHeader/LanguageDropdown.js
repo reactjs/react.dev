5f292e5ba5d29ab63bfcead141d67561deace815
Nat Alison <tesseralis@gmail.com>
2019-08-16 20:58:53 -0700

    add the dropdown

---
+++ src/components/LayoutHeader/LanguageDropdown.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+import {Menu, MenuButton, MenuList, MenuLink} from '@reach/menu-button';
+import '@reach/menu-button/styles.css';
+
+import {colors} from 'theme';
+import languages from '../../../content/languages.yml';
+
+const completeLanguages = languages.filter(language => {
+  return language.status == 2;
+});
+
+function getPageLink(code, pathname) {
+  const prefix = code === 'en' ? '' : `${code}.`;
+  return `https://${prefix}reactjs.org${pathname}`;
+}
+
+export default function LanguageDropdown({children, location}) {
+  const linkStyle = {
+    ':hover': {
+      color: `${colors.brand} !important`,
+      backgroundColor: 'transparent !important',
+    },
+    ':focus': {
+      backgroundColor: `${colors.lighter} !important`,
+    },
+  };
+  return (
+    <Menu>
+      <MenuButton
+        css={{
+          background: 'none',
+          color: 'white',
+          border: 'none',
+          cursor: 'pointer',
+        }}>
+        {children}
+      </MenuButton>
+      <MenuList
+        css={{
+          backgroundColor: `${colors.darker} !important`,
+          color: 'white !important',
+          fontSize: '1rem !important',
+          border: 'none !important',
+        }}>
+        {completeLanguages.map(lang => (
+          <MenuLink
+            css={linkStyle}
+            key={lang.code}
+            target="_blank"
+            href={getPageLink(lang.code, location.pathname)}>
+            {lang.translated_name}
+          </MenuLink>
+        ))}
+        <MenuLink css={linkStyle} as={Link} to="/languages">
+          More languages
+        </MenuLink>
+      </MenuList>
+    </Menu>
+  );
+}

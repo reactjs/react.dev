241ec7be214b39793f250cddbd67ccb4b4125637
Gasim Gasimzada <gasim@appristas.io>
2019-09-05 11:16:18 +0400

    Translate children of React elements in Translatable
    - This functionality only works one level deep
    - Translate acknowledgements

--- src/components/Translatable/Translatable.js
+++ src/components/Translatable/Translatable.js
@@ -6,8 +6,39 @@
  */
 
 import {translate} from './translate';
+import type {Node} from 'react';
+import {Children, isValidElement} from 'react';
 
-const Translatable = ({children}: {children: string}) => {
+const translateArray = childArray => {
+  let idx = 0;
+  const cleaned = childArray.map(child => {
+    if (isValidElement(child)) {
+      return `<${idx}>${child.props.children}</${idx++}>`;
+    }
+    return child;
+  });
+
+  const cleanedStr = cleaned.join('');
+  const translatedText = translate(cleanedStr).split(
+    /(<[0-9]+>.*?<\/[0-9]>)/gm,
+  );
+
+  const validElements = childArray.filter(isValidElement);
+  return translatedText.map(tt => {
+    const matches = tt.match(/<([0-9]+)>(.*?)<\/[0-9]>/m);
+    if (matches === null) {
+      return tt;
+    }
+
+    const el = validElements[matches[1]];
+    return {...el, props: {...el.props, children: matches[2]}};
+  });
+};
+
+const Translatable = ({children}: {children: Node}) => {
+  if (Array.isArray(children)) {
+    return translateArray(Children.toArray(children));
+  }
   return translate(children);
 };
 
--- src/pages/acknowledgements.html.js
+++ src/pages/acknowledgements.html.js
@@ -13,6 +13,7 @@ import {urlRoot} from 'site-constants';
 import {sharedStyles} from 'theme';
 
 import names from '../../content/acknowledgements.yml';
+import Translatable, {translate} from '../components/Translatable';
 
 const Acknowlegements = ({data, location}) => (
   <Layout location={location}>
@@ -22,11 +23,15 @@ const Acknowlegements = ({data, location}) => (
           <Header>Acknowledgements</Header>
           <TitleAndMetaTags
             canonicalUrl={`${urlRoot}/acknowledgements.html`}
-            title="React - Acknowledgements"
+            title={translate('React - Acknowledgements')}
           />
 
           <div css={sharedStyles.markdown}>
-            <p>We'd like to thank all of our contributors:</p>
+            <p>
+              <Translatable>
+                We'd like to thank all of our contributors:
+              </Translatable>
+            </p>
 
             <ul
               css={{
@@ -44,38 +49,52 @@ const Acknowlegements = ({data, location}) => (
               ))}
             </ul>
 
-            <p>In addition, we're grateful to</p>
+            <p>
+              <Translatable>In addition, we're grateful to</Translatable>
+            </p>
             <ul>
               <li>
-                <a href="https://github.com/jeffbski">Jeff Barczewski</a> for
-                allowing us to use the{' '}
-                <a href="https://www.npmjs.com/package/react">react</a> package
-                name on npm.
+                <Translatable>
+                  <a href="https://github.com/jeffbski">Jeff Barczewski</a> for
+                  allowing us to use the{' '}
+                  <a href="https://www.npmjs.com/package/react">react</a>{' '}
+                  package name on npm.
+                </Translatable>
               </li>
               <li>
-                <a href="https://christopheraue.net/">Christopher Aue</a> for
-                letting us use the{' '}
-                <a href="https://reactjs.com/">reactjs.com</a> domain name and
-                the <a href="https://twitter.com/reactjs">@reactjs</a> username
-                on Twitter.
+                <Translatable>
+                  <a href="https://christopheraue.net/">Christopher Aue</a> for
+                  letting us use the{' '}
+                  <a href="https://reactjs.com/">reactjs.com</a> domain name and
+                  the <a href="https://twitter.com/reactjs">@reactjs</a>{' '}
+                  username on Twitter.
+                </Translatable>
               </li>
               <li>
-                <a href="https://github.com/ProjectMoon">ProjectMoon</a> for
-                letting us use the{' '}
-                <a href="https://www.npmjs.com/package/flux">flux</a> package
-                name on npm.
+                <Translatable>
+                  <a href="https://github.com/ProjectMoon">ProjectMoon</a> for
+                  letting us use the{' '}
+                  <a href="https://www.npmjs.com/package/flux">flux</a> package
+                  name on npm.
+                </Translatable>
               </li>
               <li>
-                Shane Anderson for allowing us to use the{' '}
-                <a href="https://github.com/react">react</a> org on GitHub.
+                <Translatable>
+                  Shane Anderson for allowing us to use the{' '}
+                  <a href="https://github.com/react">react</a> org on GitHub.
+                </Translatable>
               </li>
               <li>
-                <a href="https://github.com/voronianski">Dmitri Voronianski</a>{' '}
-                for letting us use the{' '}
-                <a href="https://labs.voronianski.com/oceanic-next-color-scheme/">
-                  Oceanic Next
-                </a>{' '}
-                color scheme on this website.
+                <Translatable>
+                  <a href="https://github.com/voronianski">
+                    Dmitri Voronianski
+                  </a>{' '}
+                  for letting us use the{' '}
+                  <a href="https://labs.voronianski.com/oceanic-next-color-scheme/">
+                    Oceanic Next
+                  </a>{' '}
+                  color scheme on this website.
+                </Translatable>
               </li>
             </ul>
           </div>

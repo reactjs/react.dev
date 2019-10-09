0726327dbbf90ec37a5eec9d309882a183fcdfd8
Saurabh Daware <saurabhdaware99@gmail.com>
2019-10-09 21:35:28 +0530

    WebApp manifest added to allow users "Add to Homescreen" documentations (#2377)
    * manifest config added and src/images/react_logo.png added for icon
    
    * inverted commas fix as per linter
    
    * added legacy:true option for ios
    
    * legacy:true lint fix
    
    * added meta tags for ios and moved logo from src to static

--- gatsby-config.js
+++ gatsby-config.js
@@ -159,5 +159,18 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React Docs',
+        short_name: 'React',
+        start_url: '/',
+        background_color: '#20232a',
+        theme_color: '#20232a',
+        display: 'standalone',
+        icon: 'static/logo-512x512.png',
+        legacy: true,
+      },
+    },
   ],
 };

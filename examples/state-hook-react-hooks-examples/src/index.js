7ded5c66b37daec0ef3f22df01cce3a440dcde4e
garethx <gareth.wilson@gmail.com>
2020-01-14 14:59:44 +0000

    Add State Hook example
    Implements the State Hook example on https://reactjs.org/docs/hooks-overview.html

---
+++ examples/state-hook-react-hooks-examples/src/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Example />, document.getElementById("root"));

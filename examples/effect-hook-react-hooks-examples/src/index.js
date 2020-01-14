cf444ca0c660faa7b5356f58521a26d39150250d
garethx <gareth.wilson@gmail.com>
2020-01-14 15:08:13 +0000

    Add Effect Hook example
    Implements the Effect Hook example on https://reactjs.org/docs/hooks-overview.html

---
+++ examples/effect-hook-react-hooks-examples/src/index.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Example />, document.getElementById("root"));

98baaba0139b0e3c3250a0dc543da8d140098f0f
Brian Vaughn <bvaughn@fb.com>
2020-07-07 10:02:18 -0400

    Update copyright headers

--- src/templates/components/Sidebar/ScrollSyncSection.js
+++ src/templates/components/Sidebar/ScrollSyncSection.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the CC-BY-4.0 license found
  * in the LICENSE file in the root directory of this source tree.

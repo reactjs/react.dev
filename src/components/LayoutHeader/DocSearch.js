9c489048382560f80199e5aa148b25ed6e75227e
renatodex <renatodex@gmail.com>
2020-11-23 21:09:55 -0300

    Force Algolia search to redir to origin URL
    While publishing a PR, I've noticed that search doesn't work on Vercel
    Apps. This happens because Algolia results always default to production
    URLs. This is not the expected behavior while testing, since you can
    accidentally left the Vercel App without noticing, introducing all
    kinds of confusions.
    
    With this small change, we are always enforcing the origin URL to be
    used as the host for the redirect.
    This will work both for production an any other environment.

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -25,6 +25,10 @@ class DocSearch extends Component<{}, State> {
         apiKey: '36221914cce388c46d0420343e0bb32e',
         indexName: 'react',
         inputSelector: '#algolia-doc-search',
+        handleSelected: function(input, event, suggestion, datasetNumber, context) {
+          const suggestionUrl = new URL(suggestion.url)
+          window.location = `${window.origin}${suggestionUrl.pathname}`
+        },
       });
     } else {
       console.warn('Search has failed to load and now is being disabled');

e5541dbe45c8c5e27596f0f28a6ce5114cdbff4d
Sanket Shah <sanketshah17.ss@gmail.com>
2021-07-03 12:43:57 +0530

    Fix broken link in the MarkdownPage.js file

--- src/components/MarkdownPage/MarkdownPage.js
+++ src/components/MarkdownPage/MarkdownPage.js
@@ -132,7 +132,7 @@ const MarkdownPage = ({
                     </span>
                     <a
                       css={sharedStyles.articleLayout.editLink}
-                      href={`https://github.com/reactjs/reactjs.org/tree/main/${markdownRemark.fields.path}`}>
+                      href={`https://github.com/reactjs/reactjs.org/tree/master/${markdownRemark.fields.path}`}>
                       Edit this page
                     </a>
                   </div>

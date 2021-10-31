5a4446fad20ae80fcbfeef57f56b7ccbdf166225
Mark R. Florkowski <mark.florkowski@gmail.com>
2021-10-30 17:30:06 -0700

    add click to copy to TerminalBlock

--- beta/src/components/MDX/TerminalBlock.tsx
+++ beta/src/components/MDX/TerminalBlock.tsx
@@ -34,13 +34,30 @@ function TerminalBlock({level = 'info', children}: TerminalBlockProps) {
     message = children.props.children;
   }
 
+  const [copied, setCopied] = React.useState(false);
+  React.useEffect(() => {
+    if (copied) {
+      const timer = setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [copied]);
+
   return (
-    <div
-      className="rounded-lg bg-secondary dark:bg-gray-50 h-full"
-      translate="no">
+    <div className="rounded-lg bg-secondary dark:bg-gray-50 h-full hover:bg-gray-50 dark:hover:bg-gray-500">
       <div className="bg-gray-90 dark:bg-gray-60 w-full rounded-t-lg">
-        <div className="text-primary-dark dark:text-primary-dark flex text-sm px-4 py-0.5 relative">
-          <IconTerminal className="inline-flex mr-2 self-center" /> Terminal
+        <div className="text-primary-dark dark:text-primary-dark flex text-sm px-4 py-0.5 relative justify-between">
+          <div>
+            <IconTerminal className="inline-flex mr-2 self-center" /> Terminal
+          </div>
+          <div
+            className={`inline-flex ml-2 self-center opacity-1 ${
+              !copied && 'opacity-0 duration-500 transition-opacity'
+            }`}
+          >
+            Copied to clipboard!
+          </div>
         </div>
       </div>
       <div className="px-8 pt-4 pb-6 text-primary-dark dark:text-primary-dark font-mono text-code whitespace-pre">

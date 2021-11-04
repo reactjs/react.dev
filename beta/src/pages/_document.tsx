5ba0ad88b883f1569d8dc8a0bf119a7d1d2e2df4
harish-sethuraman <harish.sethuraman@freshworks.com>
2021-11-05 00:44:50 +0530

    Take system's theme for the beta site (#4022)
    * Take system's theme for the site
    
    * Add IIFE for dark mode in document load and preserve in localStorage
    
    * Add types
    
    * Fix SSR and respect override
    
    Co-authored-by: Dan Abramov <dan.abramov@me.com>

--- beta/src/pages/_document.tsx
+++ beta/src/pages/_document.tsx
@@ -12,6 +12,49 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body className="font-sans antialiased text-lg bg-wash dark:bg-wash-dark text-secondary dark:text-secondary-dark leading-base">
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function () {
+                  function setTheme(newTheme) {
+                    window.__theme = newTheme;
+                    if (newTheme === 'dark') {
+                      document.documentElement.classList.add('dark');
+                    } else if (newTheme === 'light') {
+                      document.documentElement.classList.remove('dark');
+                    }
+                  }
+
+                  var preferredTheme;
+                  try {
+                    preferredTheme = localStorage.getItem('theme');
+                  } catch (err) { }
+
+                  window.__setPreferredTheme = function(newTheme) {
+                    preferredTheme = newTheme;
+                    setTheme(newTheme);
+                    try {
+                      localStorage.setItem('theme', newTheme);
+                    } catch (err) { }
+                  };
+
+                  var initialTheme = preferredTheme;
+                  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+                  if (!initialTheme) {
+                    initialTheme = darkQuery.matches ? 'dark' : 'light';
+                  }
+                  setTheme(initialTheme);
+
+                  darkQuery.addListener(function (e) {
+                    if (!preferredTheme) {
+                      setTheme(e.matches ? 'dark' : 'light');
+                    }
+                  });
+                })();
+              `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>

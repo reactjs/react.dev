3498bff710aebe91740db7f6ecabbccfe2310c4d
Dan Abramov <dan.abramov@me.com>
2021-11-11 18:53:49 +0000

    Enable streaming

--- beta/src/pages/_document.tsx
+++ beta/src/pages/_document.tsx
@@ -3,64 +3,60 @@
  */
 
 import * as React from 'react';
-import Document, {Html, Head, Main, NextScript} from 'next/document';
+import {Html, Head, Main, NextScript} from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    //  @todo specify language in HTML?
-    return (
-      <Html lang="en">
-        <Head />
-        <body className="font-sans antialiased text-lg bg-wash dark:bg-wash-dark text-secondary dark:text-secondary-dark leading-base">
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function () {
-                  function setTheme(newTheme) {
-                    window.__theme = newTheme;
-                    if (newTheme === 'dark') {
-                      document.documentElement.classList.add('dark');
-                    } else if (newTheme === 'light') {
-                      document.documentElement.classList.remove('dark');
-                    }
+export default function Document() {
+  //  @todo specify language in HTML?
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="font-sans antialiased text-lg bg-wash dark:bg-wash-dark text-secondary dark:text-secondary-dark leading-base">
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                function setTheme(newTheme) {
+                  window.__theme = newTheme;
+                  if (newTheme === 'dark') {
+                    document.documentElement.classList.add('dark');
+                  } else if (newTheme === 'light') {
+                    document.documentElement.classList.remove('dark');
                   }
+                }
 
-                  var preferredTheme;
+                var preferredTheme;
+                try {
+                  preferredTheme = localStorage.getItem('theme');
+                } catch (err) { }
+
+                window.__setPreferredTheme = function(newTheme) {
+                  preferredTheme = newTheme;
+                  setTheme(newTheme);
                   try {
-                    preferredTheme = localStorage.getItem('theme');
+                    localStorage.setItem('theme', newTheme);
                   } catch (err) { }
+                };
 
-                  window.__setPreferredTheme = function(newTheme) {
-                    preferredTheme = newTheme;
-                    setTheme(newTheme);
-                    try {
-                      localStorage.setItem('theme', newTheme);
-                    } catch (err) { }
-                  };
+                var initialTheme = preferredTheme;
+                var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-                  var initialTheme = preferredTheme;
-                  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                if (!initialTheme) {
+                  initialTheme = darkQuery.matches ? 'dark' : 'light';
+                }
+                setTheme(initialTheme);
 
-                  if (!initialTheme) {
-                    initialTheme = darkQuery.matches ? 'dark' : 'light';
+                darkQuery.addListener(function (e) {
+                  if (!preferredTheme) {
+                    setTheme(e.matches ? 'dark' : 'light');
                   }
-                  setTheme(initialTheme);
-
-                  darkQuery.addListener(function (e) {
-                    if (!preferredTheme) {
-                      setTheme(e.matches ? 'dark' : 'light');
-                    }
-                  });
-                })();
-              `,
-            }}
-          />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+                });
+              })();
+            `,
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;

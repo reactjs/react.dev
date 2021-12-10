c5ebd6115b5c5b33172650c5b18cf65ec94161d3
Danilo Woznica <danilowoz@gmail.com>
2021-12-10 09:45:59 +0000

    update csb button

--- beta/src/components/MDX/Sandpack/OpenInCodeSandboxButton.tsx
+++ beta/src/components/MDX/Sandpack/OpenInCodeSandboxButton.tsx
@@ -3,22 +3,17 @@
  */
 
 import * as React from 'react';
-// import {useCodeSandboxLink} from '@codesandbox/sandpack-react';
+import {UnstyledOpenInCodeSandboxButton} from '@codesandbox/sandpack-react';
 import cn from 'classnames';
 import {IconNewPage} from '../../Icon/IconNewPage';
 
 export const OpenInCodeSandboxButton = ({className}: {className?: string}) => {
-  // const url = useCodeSandboxLink();
-
   return (
-    <a
+    <UnstyledOpenInCodeSandboxButton
       className={cn(
         'text-sm text-primary dark:text-primary-dark inline-flex items-center hover:text-link duration-100 ease-in transition mx-1',
         className
       )}
-      // href={url}
-      rel="noreferrer noopener"
-      target="_blank"
       title="Open in CodeSandbox">
       <span className="hidden md:inline">
         <IconNewPage className="inline mb-0.5 text-base" /> Fork
@@ -26,6 +21,6 @@ export const OpenInCodeSandboxButton = ({className}: {className?: string}) => {
       <span className="inline md:hidden">
         <IconNewPage className="inline mb-0.5 text-base" /> Fork
       </span>
-    </a>
+    </UnstyledOpenInCodeSandboxButton>
   );
 };

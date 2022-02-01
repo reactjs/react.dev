6627fc28a84db193aaeb7b1cc3de32b6f2bd327f
harish-sethuraman <harish.sethuraman@freshworks.com>
2022-02-01 21:55:52 +0530

    Lazy load codeblock alone

--- beta/src/components/MDX/CodeBlock/index.tsx
+++ beta/src/components/MDX/CodeBlock/index.tsx
@@ -1,7 +1,18 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- */
+import * as React from 'react';
+import dynamic from 'next/dynamic';
+const CodeBlock = dynamic(() => import('./CodeBlock'), {
+  suspense: true,
+});
 
-import CodeBlock from './CodeBlock';
-
-export default CodeBlock;
+export default function CodeBlockWrapper(props: any): any {
+  return (
+    <React.Suspense
+      fallback={
+        <div className="h-[406px] animate-pulse w-full shadow-lg dark:shadow-lg-dark rounded-lg dark:bg-[#16181d] bg-white">
+          <div className="bg-wash dark:bg-card-dark h-[39px] border-b border-border dark:border-border-dark rounded-t-lg rounded-b-none"></div>
+        </div>
+      }>
+      <CodeBlock {...props} />
+    </React.Suspense>
+  );
+}

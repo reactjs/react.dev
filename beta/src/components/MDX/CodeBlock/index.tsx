4cd4e3187cec362820510e5cd6a5d2a356a8839d
Dan Abramov <dan.abramov@gmail.com>
2022-02-01 05:11:40 +0000

    oops

--- beta/src/components/MDX/CodeBlock/index.tsx
+++ beta/src/components/MDX/CodeBlock/index.tsx
@@ -8,13 +8,15 @@ const CodeBlock = dynamic(() => import('./CodeBlock'), {
   suspense: true,
 });
 
-export default function (props) {
-  <React.Suspense
-    fallback={
-      <div className="h-[406px] animate-pulse w-full shadow-lg dark:shadow-lg-dark rounded-lg dark:bg-[#16181d] bg-white">
-        <div className="bg-wash dark:bg-card-dark h-[39px] border-b border-border dark:border-border-dark rounded-t-lg rounded-b-none"></div>
-      </div>
-    }>
-    <CodeBlock {...props} />
-  </React.Suspense>;
+export default function CodeBlockWrapper(props: any): any {
+  return (
+    <React.Suspense
+      fallback={
+        <div className="h-[406px] animate-pulse w-full shadow-lg dark:shadow-lg-dark rounded-lg dark:bg-[#16181d] bg-white">
+          <div className="bg-wash dark:bg-card-dark h-[39px] border-b border-border dark:border-border-dark rounded-t-lg rounded-b-none"></div>
+        </div>
+      }>
+      <CodeBlock {...props} />
+    </React.Suspense>
+  );
 }

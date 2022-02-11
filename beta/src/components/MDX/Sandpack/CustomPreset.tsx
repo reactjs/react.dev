3ffcce6a9fd6df817229ef11df3431a0692a9d5e
Dan Lebowitz <dan.lebo@me.com>
2022-02-11 04:41:20 +0000

    [Beta] Fix layout shift from previews

--- beta/src/components/MDX/Sandpack/CustomPreset.tsx
+++ beta/src/components/MDX/Sandpack/CustomPreset.tsx
@@ -73,7 +73,8 @@ export function CustomPreset({
             <SandpackCodeEditor
               customStyle={{
                 height: getHeight(),
-                maxHeight: isExpanded ? '' : 406,
+                maxHeight: isExpanded ? '' : '40vh',
+                minHeight: isExpanded ? '' : 'min-h-40vh xl:min-h-0',
               }}
               showLineNumbers
               showInlineErrors
@@ -81,7 +82,7 @@ export function CustomPreset({
             />
             <Preview
               isExpanded={isExpanded}
-              className="order-last xl:order-2"
+              className="order-last min-h-40vh xl:order-2 xl:min-h-0"
               customStyle={{
                 height: getHeight(),
                 maxHeight: isExpanded ? '' : 406,

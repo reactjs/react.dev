f8dab8265dea3dbe59c4ed141c1fa486b0e40f2d
Sha Mwe La <shamwela2002@gmail.com>
2022-02-14 23:25:10 +0630

    Remove unnecessary ARIA label

--- beta/src/components/Search.tsx
+++ beta/src/components/Search.tsx
@@ -103,7 +103,6 @@ export const Search: React.FC<SearchProps> = ({
       </button>
 
       <button
-        aria-label="Search"
         type="button"
         className="hidden md:flex relative pl-4 pr-0.5 py-1 h-10 bg-secondary-button dark:bg-gray-80 outline-none focus:ring focus:outline-none betterhover:hover:bg-opacity-80 pointer items-center shadow-inner text-left w-full text-gray-30 rounded-lg align-middle text-sm"
         onClick={onOpen}>

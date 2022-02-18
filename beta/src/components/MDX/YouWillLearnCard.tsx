e1c26726f6295bfc686c42cb466470ab548dac84
harish-sethuraman <harish.sethuraman@freshworks.com>
2022-02-18 10:15:53 +0530

    Initial Commit

--- beta/src/components/MDX/YouWillLearnCard.tsx
+++ beta/src/components/MDX/YouWillLearnCard.tsx
@@ -14,7 +14,7 @@ interface YouWillLearnCardProps {
 
 function YouWillLearnCard({title, path, children}: YouWillLearnCardProps) {
   return (
-    <div className="flex flex-col h-full bg-card dark:bg-card-dark shadow-inner justify-between rounded-lg pb-8 p-6 xl:p-8">
+    <div className="flex flex-col h-full bg-card dark:bg-card-dark shadow-inner justify-between rounded-lg pb-8 p-6 xl:p-8 mt-3">
       <div>
         <h4 className="text-primary dark:text-primary-dark font-bold text-2xl leading-tight">
           {title}

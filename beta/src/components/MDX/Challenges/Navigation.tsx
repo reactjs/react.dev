2310e15532aba273d713996a4c6ef04247dff764
Sha Mwe La <62544170+shamwela@users.noreply.github.com>
2022-02-20 05:30:54 +0630

    Add ARIA labels on the navigation buttons (#4363)

--- beta/src/components/MDX/Challenges/Navigation.tsx
+++ beta/src/components/MDX/Challenges/Navigation.tsx
@@ -108,6 +108,7 @@ export function Navigation({
       <div className="flex z-10 pb-2 pl-2">
         <button
           onClick={handleScrollLeft}
+          aria-label="Scroll left"
           className={cn(
             'bg-secondary-button dark:bg-secondary-button-dark h-8 px-2 rounded-l border-gray-20 border-r',
             {
@@ -119,6 +120,7 @@ export function Navigation({
         </button>
         <button
           onClick={handleScrollRight}
+          aria-label="Scroll right"
           className={cn(
             'bg-secondary-button dark:bg-secondary-button-dark h-8 px-2 rounded-r-lg',
             {

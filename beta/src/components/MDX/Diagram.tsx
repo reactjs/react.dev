8b0b0326f9efd38f3d5a44f5699e6f4d6cc66957
Rick Hanlon <rickhanlonii@gmail.com>
2022-02-23 22:00:59 -0500

    Switch dark mode image

--- beta/src/components/MDX/Diagram.tsx
+++ beta/src/components/MDX/Diagram.tsx
@@ -16,12 +16,22 @@ interface DiagramProps {
 export function Diagram({name, alt, height, width, children}: DiagramProps) {
   return (
     <figure className="flex flex-col px-0 py-5 sm:p-10">
-      <Image
-        src={`/images/docs/diagrams/${name}`}
-        alt={alt}
-        height={height}
-        width={width}
-      />
+      <div className="dark-image">
+        <Image
+          src={`/images/docs/diagrams/dark_${name}`}
+          alt={alt}
+          height={height}
+          width={width}
+        />
+      </div>
+      <div className="light-image">
+        <Image
+          src={`/images/docs/diagrams/${name}`}
+          alt={alt}
+          height={height}
+          width={width}
+        />
+      </div>
       <figcaption className="p-1 sm:p-4 mt-4 sm:mt-0 text-gray-40 text-base lg:text-lg text-center leading-6">
         {children}
       </figcaption>

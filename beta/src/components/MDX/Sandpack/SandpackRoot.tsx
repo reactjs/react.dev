abee2a67133daf31f63e64e664ee7c45500a6d80
Strek <ssharishkumar@gmail.com>
2022-05-31 22:13:04 +0530

    update bundler URL

--- beta/src/components/MDX/Sandpack/SandpackRoot.tsx
+++ beta/src/components/MDX/Sandpack/SandpackRoot.tsx
@@ -91,7 +91,7 @@ function SandpackRoot(props: SandpackProps) {
         autorun={autorun}
         initMode="user-visible"
         initModeObserverOptions={{rootMargin: '1400px 0px'}}
-        bundlerURL="https://22530bfe.sandpack-bundler.pages.dev"
+        bundlerURL="https://f13a87c3.sandpack-bundler.pages.dev"
         logLevel={SandpackLogLevel.None}>
         <CustomPreset
           isSingleFile={isSingleFile}

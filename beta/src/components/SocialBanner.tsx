1b4f7a92ec014fedcbdb8d498260c191a81608ba
Нурбек Азамат уулу <nurbek@MacBook-Air-2.local>
2022-06-02 08:18:39 +0600

    save

--- beta/src/components/SocialBanner.tsx
+++ beta/src/components/SocialBanner.tsx
@@ -3,7 +3,7 @@
  *
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ExternalLink} from './ExternalLink';
 
 // TODO: Unify with the old site settings.
@@ -13,8 +13,20 @@ const bannerLink = 'https://opensource.fb.com/support-ukraine';
 const bannerLinkText = 'Help Provide Humanitarian Aid to Ukraine.';
 
 export default function SocialBanner() {
+  const [showBanner, setShowBanner] = useState(true);
+  const updateVisibility = () => setShowBanner(window.scrollY <= 6);
+  useEffect(() => {
+    setShowBanner(window.scrollY <= 6);
+    window.addEventListener('scroll', updateVisibility);
+    return () => window.removeEventListener('scroll', updateVisibility);
+  }, []);
   return (
-    <div className="w-full bg-gray-100 dark:bg-gray-700 fixed py-2 h-16 sm:h-10 sm:py-0 flex items-center justify-center flex-col sm:flex-row z-[100]">
+    <div
+      className={
+        showBanner
+          ? `w-full bg-gray-100 dark:bg-gray-700 fixed py-2 h-16 sm:h-10 sm:py-0 flex items-center justify-center flex-col sm:flex-row z-[100]`
+          : 'hidden'
+      }>
       {bannerText}
       <ExternalLink
         className="ml-0 sm:ml-1 text-link dark:text-link-dark hover:underline"

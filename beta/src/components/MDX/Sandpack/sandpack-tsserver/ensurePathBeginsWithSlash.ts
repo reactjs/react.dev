6a3dc050d3eedc543616695e7940bbc7249fc0a1
Jake Teton-Landis <just.1.jake@gmail.com>
2022-06-03 06:39:19 -0700

    Update beta/src/components/MDX/Sandpack/sandpack-tsserver/ensurePathBeginsWithSlash.ts
    Co-authored-by: Danilo Woznica <danilowoz@gmail.com>

--- beta/src/components/MDX/Sandpack/sandpack-tsserver/ensurePathBeginsWithSlash.ts
+++ beta/src/components/MDX/Sandpack/sandpack-tsserver/ensurePathBeginsWithSlash.ts
@@ -2,11 +2,8 @@ import type {SandpackFiles} from '@codesandbox/sandpack-react';
 
 export function ensurePathStartsWithSlash(path: string): string;
 export function ensurePathStartsWithSlash(path: undefined): undefined;
-export function ensurePathStartsWithSlash(path: string | undefined) {
-  if (path === undefined) {
-    return path;
-  }
-  if (path[0] === '/') {
+export function ensurePathStartsWithSlash(path?: string) {
+  if (path === undefined || path[0] === '/') {
     return path;
   }
   return `/${path}`;

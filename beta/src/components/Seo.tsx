75cf2e66fece9e6ff525597926eac5a48e69179b
Strek <ssharishkumar@gmail.com>
2022-08-01 13:33:10 +0530

    fix types

--- beta/src/components/Seo.tsx
+++ beta/src/components/Seo.tsx
@@ -32,88 +32,85 @@ export const Seo = withRouter(
     children,
   }: SeoProps & {router: Router}) => (
     <Head>
-      {/* DEFAULT */}
-
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-      {title != null && <title key="title">{title}</title>}
-      {description != null && (
-        <meta name="description" key="description" content={description} />
-      )}
-      {console.log}
-      {/* <link rel="icon" type="image/x-icon" href={favicon} />
+      <>
+        {' '}
+        {/* DEFAULT */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {title != null && <title key="title">{title}</title>}
+        {description != null && (
+          <meta name="description" key="description" content={description} />
+        )}
+        {console.log}
+        {/* <link rel="icon" type="image/x-icon" href={favicon} />
       <link rel="apple-touch-icon" href={favicon} />  @todo favicon */}
-      <meta property="fb:app_id" content="623268441017527" />
-      {/* OPEN GRAPH */}
-      <meta
-        property="og:image"
-        content={`${apiURL}/api?description=${description}&title=${title}`}
-      />
-
-      <meta property="og:type" key="og:type" content="website" />
-      <meta
-        property="og:url"
-        key="og:url"
-        content={`https://beta.reactjs.org${router.pathname}`}
-      />
-
-      {title != null && (
-        <meta property="og:title" content={title} key="og:title" />
-      )}
-      {description != null && (
+        <meta property="fb:app_id" content="623268441017527" />
+        {/* OPEN GRAPH */}
         <meta
-          property="og:description"
-          key="og:description"
-          content={description}
+          property="og:image"
+          content={`${apiURL}/api?description=${description}&title=${title}`}
         />
-      )}
-
-      {/* TWITTER */}
-      <meta
-        name="twitter:card"
-        key="twitter:card"
-        content="summary_large_image"
-      />
-      <meta name="twitter:site" key="twitter:site" content="@reactjs" />
-      <meta name="twitter:creator" key="twitter:creator" content="@reactjs" />
-      {title != null && (
-        <meta name="twitter:title" key="twitter:title" content={title} />
-      )}
-      {description != null && (
+        <meta property="og:type" key="og:type" content="website" />
         <meta
-          name="twitter:description"
-          key="twitter:description"
-          content={description}
+          property="og:url"
+          key="og:url"
+          content={`https://beta.reactjs.org${router.pathname}`}
         />
-      )}
-
-      <meta
-        name="twitter:image"
-        key="twitter:image"
-        content={`https://beta.reactjs.org${image}`}
-      />
-      <link
-        rel="preload"
-        href="/fonts/Source-Code-Pro-Regular.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="https://beta.reactjs.org/fonts/Optimistic_Display_W_Md.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="https://beta.reactjs.org/fonts/Optimistic_Display_W_Bd.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      {children}
+        {title != null && (
+          <meta property="og:title" content={title} key="og:title" />
+        )}
+        {description != null && (
+          <meta
+            property="og:description"
+            key="og:description"
+            content={description}
+          />
+        )}
+        {/* TWITTER */}
+        <meta
+          name="twitter:card"
+          key="twitter:card"
+          content="summary_large_image"
+        />
+        <meta name="twitter:site" key="twitter:site" content="@reactjs" />
+        <meta name="twitter:creator" key="twitter:creator" content="@reactjs" />
+        {title != null && (
+          <meta name="twitter:title" key="twitter:title" content={title} />
+        )}
+        {description != null && (
+          <meta
+            name="twitter:description"
+            key="twitter:description"
+            content={description}
+          />
+        )}
+        <meta
+          name="twitter:image"
+          key="twitter:image"
+          content={`https://beta.reactjs.org${image}`}
+        />
+        <link
+          rel="preload"
+          href="/fonts/Source-Code-Pro-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://beta.reactjs.org/fonts/Optimistic_Display_W_Md.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://beta.reactjs.org/fonts/Optimistic_Display_W_Bd.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        {children}
+      </>
     </Head>
   )
 );

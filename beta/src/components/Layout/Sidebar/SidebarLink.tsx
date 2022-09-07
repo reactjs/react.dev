f1cdb194bd0d7f3bd4bddfad0645aee35fdc0635
Dan Abramov <dan.abramov@gmail.com>
2022-09-07 04:44:52 +0100

    [Beta] Panel fixes

--- beta/src/components/Layout/Sidebar/SidebarLink.tsx
+++ beta/src/components/Layout/Sidebar/SidebarLink.tsx
@@ -68,7 +68,7 @@ export function SidebarLink({
               !selected && !heading,
             'text-base text-link dark:text-link-dark bg-highlight dark:bg-highlight-dark border-blue-40 hover:bg-highlight hover:text-link dark:hover:bg-highlight-dark dark:hover:text-link-dark':
               selected,
-            'dark:bg-gray-60 bg-gray-3 dark:hover:bg-gray-60 hover:bg-gray-3':
+            'dark:bg-gray-70 bg-gray-3 dark:hover:bg-gray-70 hover:bg-gray-3':
               isPending,
           }
         )}>

eb64b2dfaa6873ea22ad976bc7033e12a049645e
Rick Hanlon <rickhanlonii@fb.com>
2022-11-30 16:51:29 -0500

    [wip] update team page

---
+++ beta/src/components/MDX/TeamMember.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ */
+
+import * as React from 'react';
+import Image from 'next/Image';
+import {IconTwitter} from '../Icon/IconTwitter';
+import {IconGitHub} from '../Icon/IconGitHub';
+import {ExternalLink} from '../ExternalLink';
+
+interface TeamMemberProps {
+  name: string;
+  children: React.ReactNode;
+  photo: string;
+  twitter?: string;
+  github?: string;
+  personal?: string;
+}
+
+// TODO: good alt text for images/links
+export function TeamMember({
+  children,
+  photo,
+  github,
+  twitter,
+  personal,
+}: TeamMemberProps) {
+  return (
+    <div className="flex">
+      <div
+        className="basis-2/5 rounded overflow-hidden relative"
+        style={{width: 300, height: 250}}>
+        <Image src={photo} layout="fill" objectFit="cover" alt="TODO" />
+      </div>
+      <div className="pl-6 basis-3/5 items-start">
+        {children}
+        <div className="flex flex-row">
+          <div className="mr-4">
+            <ExternalLink
+              aria-label="React on Twitter"
+              href={`https://twitter.com/${twitter}`}
+              className="hover:text-primary dark:text-primary-dark flex flex-row items-center">
+              <IconTwitter />
+              {twitter}
+            </ExternalLink>
+          </div>
+          <div className="mr-4">
+            <ExternalLink
+              aria-label="React on Twitter"
+              href={`https://twitter.com/${github}`}
+              className="hover:text-primary dark:text-primary-dark flex flex-row items-center">
+              <IconGitHub /> {github}
+            </ExternalLink>
+          </div>
+          {personal && (
+            <ExternalLink
+              aria-label="React on Twitter"
+              href={`https://${personal}`}
+              className="hover:text-primary dark:text-primary-dark flex flex-row items-center">
+              {personal}
+            </ExternalLink>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}

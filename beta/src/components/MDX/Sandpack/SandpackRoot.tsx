cf6ee24b9312207b751fcaebb76f01afab5ff7fa
dan <dan.abramov@gmail.com>
2022-12-05 17:40:20 +0000

    [Beta] Update the bundler

--- beta/src/components/MDX/Sandpack/SandpackRoot.tsx
+++ beta/src/components/MDX/Sandpack/SandpackRoot.tsx
@@ -87,7 +87,7 @@ function SandpackRoot(props: SandpackProps) {
           autorun,
           initMode: 'user-visible',
           initModeObserverOptions: {rootMargin: '1400px 0px'},
-          bundlerURL: 'https://94be751e.sandpack-bundler.pages.dev',
+          bundlerURL: 'https://dad0ba0e.sandpack-bundler-4bw.pages.dev',
           logLevel: SandpackLogLevel.None,
         }}>
         <CustomPreset

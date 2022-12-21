befa7cdd6d9db4a04ca67045d24ed3cf6aaef4b6
Asad Ashraf <asadashraf@Asads-MacBook-Pro.local>
2022-12-22 01:22:38 +0500

    404 is shown for 1 second before matching client paths

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,25 +15,31 @@ import {sharedStyles} from 'theme';
 type Props = {
   location: Location,
 };
-
-const PageNotFound = ({location}: Props) => (
-  <Layout location={location}>
-    <Container>
-      <div css={sharedStyles.articleLayout.container}>
-        <div css={sharedStyles.articleLayout.content}>
-          <Header>Page Not Found</Header>
-          <TitleAndMetaTags title="React - Page Not Found" />
-          <div css={sharedStyles.markdown}>
-            <p>We couldn't find what you were looking for.</p>
-            <p>
-              Please contact the owner of the site that linked you to the
-              original URL and let them know their link is broken.
-            </p>
-          </div>
-        </div>
-      </div>
-    </Container>
-  </Layout>
-);
+const PageNotFound = ({location}: Props) => {
+  const browser = typeof window !== 'undefined' && window;
+  return (
+    <div>
+      {browser && (
+        <Layout location={location}>
+          <Container>
+            <div css={sharedStyles.articleLayout.container}>
+              <div css={sharedStyles.articleLayout.content}>
+                <Header>Page Not Found</Header>
+                <TitleAndMetaTags title="React - Page Not Found" />
+                <div css={sharedStyles.markdown}>
+                  <p>We couldn't find what you were looking for.</p>
+                  <p>
+                    Please contact the owner of the site that linked you to the
+                    original URL and let them know their link is broken.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </Layout>
+      )}
+    </div>
+  );
+};
 
 export default PageNotFound;

3be58c71a210c6e8f4a1a99fb8c13eecd633dd6d
D <dan.lebo@me.com>
2023-02-06 20:36:37 -0800

    blog card tweaks

--- beta/src/components/MDX/BlogCard.tsx
+++ beta/src/components/MDX/BlogCard.tsx
@@ -15,7 +15,7 @@ export interface BlogCardProps {
 function BlogCard({title, date, url, children}: BlogCardProps) {
   return (
     <Link href={url as string}>
-      <div className="-mx-2.5 sm:-mx-6 my-6 p-5 sm:p-6 cursor-pointer bg-card group dark:bg-card-dark rounded-2xl text-xl text-primary dark:text-primary-dark leading-relaxed">
+      <div className="-mx-2.5 sm:-mx-6 my-6 p-5 sm:p-6 cursor-pointer border-border border dark:border-border-dark hover:bg-card group hover:dark:bg-card-dark rounded-2xl text-xl text-primary dark:text-primary-dark leading-relaxed">
         <h2 className="font-bold text-3xl group-hover:underline leading-snug mb-4">
           {title}
         </h2>

31c69ed78c310d7bfce359b10f334becb0bd8866
Dan Lebo <dan.lebo@me.com>
2023-02-14 11:31:38 -0800

    8 images

--- beta/src/components/Layout/HomeContent.js
+++ beta/src/components/Layout/HomeContent.js
@@ -380,11 +380,11 @@ export function HomeContent() {
                 </div>
               </div>
             </div>
-            <div className="relative flex overflow-x-hidden w-auto">
+            <div className="relative flex overflow-hidden w-auto">
               <div className="w-full py-12 animate-marquee whitespace-nowrap flex flex-row">
                 <CommunityImages />
               </div>
-              <div className="w-full absolute left-0 right-0 top-0 py-12 animate-marquee2 whitespace-nowrap flex flex-row">
+              <div className="w-full absolute top-0 py-12 animate-marquee2 whitespace-nowrap flex flex-row">
                 <CommunityImages />
               </div>
             </div>
@@ -437,6 +437,10 @@ const images = [
   reactConf2021Cover,
   reactConf2019Cover,
   reactConf2021Cover,
+  reactConf2019Cover,
+  reactConf2021Cover,
+  reactConf2019Cover,
+  reactConf2021Cover,
 ];
 
 function CommunityImages() {
@@ -445,7 +449,7 @@ function CommunityImages() {
       {images.map((src, i) => (
         <div
           key={i}
-          className="flex justify-center px-2.5 lg:px-5 w-6/12 lg:w-3/12">
+          className="flex justify-center px-2.5 lg:px-5 min-w-[50%] lg:min-w-[25%]">
           <img
             src={src}
             className={`rounded-xl object-cover rotate-${

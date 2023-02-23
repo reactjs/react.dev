4ed58ffd44fc69f8e0f88ea080f9281203332020
Marcos Bergamo <Marcos.Bergamo@refinitiv.com>
2023-02-23 09:41:39 +0100

    feat(beta): add edit on github for docs

--- beta/src/siteConfig.ts
+++ beta/src/siteConfig.ts
@@ -3,7 +3,7 @@
  */
 
 export const siteConfig = {
-  editUrl: 'https://github.com/reactjs/reactjs.org/edit/beta/src/pages',
+  editUrl: 'https://github.com/reactjs/reactjs.org/edit/main/beta/src/pages',
   copyright: `Copyright © ${new Date().getFullYear()} Facebook Inc. All Rights Reserved.`,
   repoUrl: 'https://github.com/facebook/react',
   twitterUrl: 'https://twitter.com/reactjs',

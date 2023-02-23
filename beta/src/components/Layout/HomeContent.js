bca2920215687e1afaf19a4f2e30fa992b2c18be
Dan Abramov <dan.abramov@gmail.com>
2023-02-23 18:40:00 +0000

    all imgs

--- beta/src/components/Layout/HomeContent.js
+++ beta/src/components/Layout/HomeContent.js
@@ -519,19 +519,13 @@ export function HomeContent() {
 const reactConf2021Cover = 'https://i.imgur.com/TsMgXlX.jpg';
 const reactConf2019Cover = 'https://i.imgur.com/YDWtkrY.jpg';
 const images = [
-  // 'https://i.imgur.com/QJmZSEq.jpg', // react conf nat
+  'https://i.imgur.com/QJmZSEq.jpg', // react conf nat
   'https://i.imgur.com/LXQqeAc.jpg', // react india sunil
-  // 'https://i.imgur.com/xDLyt24.jpg', // react conf hallway
+  'https://i.imgur.com/xDLyt24.jpg', // react conf hallway
   'https://i.imgur.com/e8M5gVl.jpg', // react india audience
-  // 'https://i.imgur.com/hNuzz36.jpg', // react conf elizabet
+  'https://i.imgur.com/hNuzz36.jpg', // react conf elizabet
   'https://i.imgur.com/PfxTBy9.jpg', // react india selfie
-  // 'https://i.imgur.com/RX96gix.jpg', // react conf fun
-  'https://i.imgur.com/4po0zdV.jpg', // react india table
-
-  // TODO: remove repetition after we license other images.
-  'https://i.imgur.com/LXQqeAc.jpg', // react india sunil
-  'https://i.imgur.com/e8M5gVl.jpg', // react india audience
-  'https://i.imgur.com/PfxTBy9.jpg', // react india selfie
+  'https://i.imgur.com/Xz4hMUC.jpg', // react conf fun
   'https://i.imgur.com/4po0zdV.jpg', // react india table
 ];
 

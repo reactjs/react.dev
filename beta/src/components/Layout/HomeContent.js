09b83cd941077e2c7173f5fc5516e843501742f3
Dan Abramov <dan.abramov@gmail.com>
2023-02-27 23:42:17 +0000

    eager lazyg

--- beta/src/components/Layout/HomeContent.js
+++ beta/src/components/Layout/HomeContent.js
@@ -488,16 +488,7 @@ export function HomeContent() {
                 </Para>
               </Center>
             </div>
-            <div className="relative flex overflow-x-hidden overflow-y-visible w-auto">
-              <div className="w-full py-12 lg:py-20 animate-marquee lg:animate-large-marquee whitespace-nowrap flex flex-row">
-                <CommunityImages />
-              </div>
-              <div
-                aria-hidden="true"
-                className="w-full absolute top-0 py-12 lg:py-20 animate-marquee2 lg:animate-large-marquee2 whitespace-nowrap flex flex-row">
-                <CommunityImages />
-              </div>
-            </div>
+            <CommunityGallery />
             <div className="mx-auto flex flex-col max-w-4xl">
               <Center>
                 <Para>
@@ -532,43 +523,6 @@ export function HomeContent() {
     </>
   );
 }
-
-const reactConf2021Cover = '/images/home/conf2021/cover.svg';
-const reactConf2019Cover = '/images/home/conf2019/cover.svg';
-const images = [
-  {
-    src: '/images/home/community/react_conf_fun.webp',
-    alt: 'People singing karaoke at React Conf',
-  },
-  {
-    src: '/images/home/community/react_india_sunil.webp',
-    alt: 'Sunil Pai speaking at React India',
-  },
-  {
-    src: '/images/home/community/react_conf_hallway.webp',
-    alt: 'A hallway conversation between two people at React Conf',
-  },
-  {
-    src: '/images/home/community/react_india_hallway.webp',
-    alt: 'A hallway conversation at React India',
-  },
-  {
-    src: '/images/home/community/react_conf_elizabet.webp',
-    alt: 'Elizabet Oliveira speaking at React Conf',
-  },
-  {
-    src: '/images/home/community/react_india_selfie.webp',
-    alt: 'People taking a group selfie at React India',
-  },
-  {
-    src: '/images/home/community/react_conf_nat.webp',
-    alt: 'Nat Alison speaking at React Conf',
-  },
-  {
-    src: '/images/home/community/react_india_team.webp',
-    alt: 'Organizers greeting attendees at React India',
-  },
-];
 
 function CTA({children, icon, href}) {
   const Tag = href.startsWith('https://') ? ExternalLink : 'a';
@@ -699,10 +653,99 @@ function CTA({children, icon, href}) {
     </Tag>
   );
 }
-function CommunityImages() {
+
+const reactConf2021Cover = '/images/home/conf2021/cover.svg';
+const reactConf2019Cover = '/images/home/conf2019/cover.svg';
+const communityImages = [
+  {
+    src: '/images/home/community/react_conf_fun.webp',
+    alt: 'People singing karaoke at React Conf',
+  },
+  {
+    src: '/images/home/community/react_india_sunil.webp',
+    alt: 'Sunil Pai speaking at React India',
+  },
+  {
+    src: '/images/home/community/react_conf_hallway.webp',
+    alt: 'A hallway conversation between two people at React Conf',
+  },
+  {
+    src: '/images/home/community/react_india_hallway.webp',
+    alt: 'A hallway conversation at React India',
+  },
+  {
+    src: '/images/home/community/react_conf_elizabet.webp',
+    alt: 'Elizabet Oliveira speaking at React Conf',
+  },
+  {
+    src: '/images/home/community/react_india_selfie.webp',
+    alt: 'People taking a group selfie at React India',
+  },
+  {
+    src: '/images/home/community/react_conf_nat.webp',
+    alt: 'Nat Alison speaking at React Conf',
+  },
+  {
+    src: '/images/home/community/react_india_team.webp',
+    alt: 'Organizers greeting attendees at React India',
+  },
+];
+
+function CommunityGallery() {
+  const [isLazy, setIsLazy] = useState(true);
+  const ref = useRef();
+
+  // Either wait until we're scrolling close...
+  useEffect(() => {
+    if (!isLazy) {
+      return;
+    }
+    const rootVertical = parseInt(window.innerHeight * 2.5);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsLazy(false);
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: `${rootVertical}px 0px`,
+      }
+    );
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [isLazy]);
+
+  // ... or until it's been a while after hydration.
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLazy(false);
+    }, 20 * 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      className="relative flex overflow-x-hidden overflow-y-visible w-auto">
+      <div className="w-full py-12 lg:py-20 animate-marquee lg:animate-large-marquee whitespace-nowrap flex flex-row">
+        <CommunityImages isLazy={isLazy} />
+      </div>
+      <div
+        aria-hidden="true"
+        className="w-full absolute top-0 py-12 lg:py-20 animate-marquee2 lg:animate-large-marquee2 whitespace-nowrap flex flex-row">
+        <CommunityImages isLazy={isLazy} />
+      </div>
+    </div>
+  );
+}
+
+function CommunityImages({isLazy}) {
   return (
     <>
-      {images.map(({src, alt}, i) => (
+      {communityImages.map(({src, alt}, i) => (
         <div
           key={i}
           className={cn(
@@ -716,7 +759,7 @@ function CommunityImages() {
                 : 'group-hover:rotate-1 group-hover:scale-110 group-hover:shadow-lg lg:group-hover:shadow-2xl rotate-[-2deg]'
             )}>
             <img
-              loading="lazy"
+              loading={isLazy ? 'lazy' : 'eager'}
               src={src}
               alt={alt}
               className="aspect-[4/3] h-full w-full flex object-cover rounded-2xl bg-gray-10 dark:bg-gray-80"

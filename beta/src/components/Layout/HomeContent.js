963cade0ff919df1bdc4b4162378e9732e7abca4
Dan Lebo <dan.lebo@me.com>
2023-03-01 12:24:12 -0800

    polish

--- beta/src/components/Layout/HomeContent.js
+++ beta/src/components/Layout/HomeContent.js
@@ -844,7 +844,7 @@ function ExampleLayout({
   return (
     <div className="lg:pl-10 lg:pr-5 w-full" ref={ref}>
       <div className="mt-12 mb-2 lg:my-16 max-w-7xl mx-auto flex flex-col w-full lg:rounded-2xl lg:bg-card lg:dark:bg-card-dark">
-        <div className="flex-col gap-0 lg:gap-5 lg:rounded-2xl lg:bg-gray-10 lg:dark:bg-gray-70 shadow-inner lg:flex-row flex grow w-full mx-auto items-center bg-cover bg-center lg:bg-right lg:bg-[length:60%_100%] bg-no-repeat bg-meta-gradient dark:bg-meta-gradient-dark">
+        <div className="flex-col gap-0 lg:gap-5 lg:rounded-2xl lg:bg-gray-10 lg:dark:bg-gray-70 shadow-inner-border dark:shadow-inner-border-dark lg:flex-row flex grow w-full mx-auto items-center bg-cover bg-center lg:bg-right lg:bg-[length:60%_100%] bg-no-repeat bg-meta-gradient dark:bg-meta-gradient-dark">
           <div className="lg:-m-5 h-full shadow-nav dark:shadow-nav-dark lg:rounded-2xl bg-wash dark:bg-gray-95 w-full flex grow flex-col">
             <div className="w-full bg-card dark:bg-wash-dark lg:rounded-t-2xl border-b border-black/5 dark:border-white/5">
               <h3 className="text-sm my-1 mx-5 text-tertiary dark:text-tertiary-dark select-none">
@@ -855,7 +855,7 @@ function ExampleLayout({
           </div>
           <div
             ref={contentRef}
-            className="relative mt-5 lg:-my-20 w-full p-2.5 xs:p-5 lg:p-10 flex grow justify-center">
+            className="relative mt-0 lg:-my-20 w-full p-2.5 xs:p-5 lg:p-10 flex grow justify-center">
             {right}
             <div
               className={cn(
@@ -1095,7 +1095,7 @@ function SearchableVideoList({ videos }) {
           <ExamplePanel
             noShadow={false}
             noPadding={true}
-            contentMarginTop="80px"
+            contentMarginTop="72px"
             height="30rem">
             <h1 className="mx-4 mb-1 font-bold text-3xl text-primary">
               React Videos

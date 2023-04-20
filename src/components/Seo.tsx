c019eb1ee360dddab4d0e3d9107d15a0a419f8d1
Dan Abramov <dan.abramov@gmail.com>
2023-04-20 19:33:01 +0100

    Add noindex for untranslated sites

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -22,7 +22,17 @@ const deployedTranslations = [
   'zh-hans',
   'es',
   // We'll add more languages when they have enough content.
+  // Please DO NOT edit this list without a discussion in the reactjs/react.dev repo.
+  // It must be the same between all translations.
 ];
+
+let shouldPreventIndexing = false;
+if (
+  siteConfig.languageCode !== 'en' &&
+  !deployedTranslations.includes(siteConfig.languageCode)
+) {
+  shouldPreventIndexing = true;
+}
 
 function getDomain(languageCode: string): string {
   const subdomain = languageCode === 'en' ? '' : languageCode + '.';
@@ -59,6 +69,7 @@ export const Seo = withRouter(
           href={canonicalUrl.replace(siteDomain, getDomain('en'))}
           hrefLang="x-default"
         />
+        {shouldPreventIndexing && <meta name="robots" content="noindex" />}
         {deployedTranslations.map((languageCode) => (
           <link
             key={'alt-' + languageCode}

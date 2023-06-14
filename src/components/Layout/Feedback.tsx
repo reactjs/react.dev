3376271b85d6fcc92c2b446135c328b3a2da7777
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-06-14 20:00:59 +0530

    Fixed according to Comments

--- src/components/Layout/Feedback.tsx
+++ src/components/Layout/Feedback.tsx
@@ -79,8 +79,11 @@ function SendFeedback({onSubmit}: {onSubmit: () => void}) {
   }, [isSubmitted]);
   return (
     <div
-      className="max-w-xs w-80 lg:w-auto py-3 shadow-lg rounded-lg m-4 bg-wash dark:bg-gray-95 px-4 flex"
-      style={{opacity: isVisible ? 1 : 0}}>
+      className="max-w-xs w-80 lg:w-auto py-3 shadow-lg rounded-lg m-4 bg-wash dark:bg-gray-95 px-4 flex transition-opacity duration-300"
+      style={{
+        display: isVisible ? 'flex' : 'none',
+        pointerEvents: isVisible ? 'auto' : 'none',
+      }}>
       <p className="w-full font-bold text-primary dark:text-primary-dark text-lg mr-4">
         {isSubmitted ? 'Thank you for your feedback!' : 'Is this page useful?'}
       </p>

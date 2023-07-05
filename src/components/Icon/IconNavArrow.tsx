df9620d944d9a47c5394b26fe4e3536ba3031333
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-07-05 17:49:57 +0300

    Refactor TW classes: Use logical properties over directional properties.

--- src/components/Icon/IconNavArrow.tsx
+++ src/components/Icon/IconNavArrow.tsx
@@ -7,15 +7,15 @@ import cn from 'classnames';
 
 export const IconNavArrow = memo<
   JSX.IntrinsicElements['svg'] & {
-    displayDirection: 'right' | 'down' | 'left';
+    displayDirection: 'right' | 'down' | 'left' | 'start' | 'end';
   }
->(function IconNavArrow({displayDirection = 'right', className}) {
+>(function IconNavArrow({displayDirection = 'start', className}) {
   const classes = cn(
     'duration-100 ease-in transition',
     {
       'rotate-0': displayDirection === 'down',
-      '-rotate-90': displayDirection === 'right',
-      'rotate-90': displayDirection === 'left',
+      '-rotate-90': displayDirection === 'end' || displayDirection === 'right',
+      'rotate-90': displayDirection === 'start' || displayDirection === 'left',
     },
     className
   );

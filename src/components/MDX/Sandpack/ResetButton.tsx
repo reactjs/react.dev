71db2eb6f72729978dc030d713496b4235e5dd3d
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-08-15 02:31:19 +0300

    chore: use mx-* instead of me-* & ms-*

--- src/components/MDX/Sandpack/ResetButton.tsx
+++ src/components/MDX/Sandpack/ResetButton.tsx
@@ -15,7 +15,7 @@ export function ResetButton({onReset}: ResetButtonProps) {
       onClick={onReset}
       title="Reset Sandbox"
       type="button">
-      <IconRestart className="inline ms-1 me-1 relative" /> Reset
+      <IconRestart className="inline mx-1 relative" /> Reset
     </button>
   );
 }

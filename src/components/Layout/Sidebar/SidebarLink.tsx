44f442d5b201c633619c1c9a5fec784891c696ae
Luna <lunaleaps@gmail.com>
2023-11-08 19:55:40 -0400

    Add space for canary title (#6416)
    Co-authored-by: Luna Wei <luwe@fb.com>

--- src/components/Layout/Sidebar/SidebarLink.tsx
+++ src/components/Layout/Sidebar/SidebarLink.tsx
@@ -77,7 +77,7 @@ export function SidebarLink({
         {title}{' '}
         {canary && (
           <IconCanary
-            title="This feature is available in the latest Canary"
+            title=" - This feature is available in the latest Canary"
             className="ms-2 text-gray-30 dark:text-gray-60 inline-block w-4 h-4 align-[-3px]"
           />
         )}

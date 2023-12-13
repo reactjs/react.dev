6c720528ce9ccf15cfc9884db45d2aa8e2cff4a4
Danilo Woznica <danilowoz@gmail.com>
2023-12-13 09:38:37 +0000

    style: Update file paths in Sandpack components
    Update file paths in Sandpack components to use root folder and variables

--- src/components/MDX/Sandpack/template.ts
+++ src/components/MDX/Sandpack/template.ts
@@ -9,9 +9,9 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );`,
   },
   '/package.json': {
@@ -42,12 +42,12 @@ root.render(
     code: `<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-	<div id="root"></div>
+  <div id="root"></div>
 </body>
 </html>`,
   },

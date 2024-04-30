c737d597b8b64691a4d479a154b8875e29acf718
Rick Hanlon <rickhanlonii@fb.com>
2024-04-29 22:25:06 -0400

    Bump version string

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -3,7 +3,7 @@
  */
 
 exports.siteConfig = {
-  version: '18.3.1',
+  version: '19-beta',
   // --------------------------------------
   // Translations should replace these lines:
   languageCode: 'en',

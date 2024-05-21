8f7450939135f3dcbc97ff7233c32044533acd8a
OH HYUN KEUN <ohhk@uitgis.com>
2024-05-21 10:17:41 +0900

    navitem-css-change

--- src/components/Layout/TopNav/TopNav.tsx
+++ src/components/Layout/TopNav/TopNav.tsx
@@ -125,7 +125,7 @@ function NavItem({url, isActive, children}: any) {
       <Link
         href={url}
         className={cn(
-          'active:scale-95 transition-transform w-full text-center outline-link py-1.5 px-1.5 xs:px-3 sm:px-4 rounded-full capitalize',
+          'active:scale-95 transition-transform w-full text-center outline-link py-1.5 px-1.5 xs:px-3 sm:px-4 rounded-full capitalize whitespace-nowrap',
           !isActive && 'hover:bg-primary/5 hover:dark:bg-primary-dark/5',
           isActive &&
             'bg-highlight dark:bg-highlight-dark text-link dark:text-link-dark'

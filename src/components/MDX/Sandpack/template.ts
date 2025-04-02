381a62deb301cc02a073023ab9ef46251505f358
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-04-02 09:08:59 +0200

    Use stable React by default in Sandpacks

--- src/components/MDX/Sandpack/template.ts
+++ src/components/MDX/Sandpack/template.ts
@@ -28,8 +28,8 @@ root.render(
           eject: 'react-scripts eject',
         },
         dependencies: {
-          react: '19.0.0-rc-3edc000d-20240926',
-          'react-dom': '19.0.0-rc-3edc000d-20240926',
+          react: '^19.1.0',
+          'react-dom': '^19.1.0',
           'react-scripts': '^5.0.0',
         },
       },

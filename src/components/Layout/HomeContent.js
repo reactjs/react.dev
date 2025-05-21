8669c3d2a92da92154fd38a3e02dfc246569d607
nannany <ymym1990ymym@gmail.com>
2025-05-21 13:54:04 +0900

    Fix NavContext usage in Talks component

--- src/components/Layout/HomeContent.js
+++ src/components/Layout/HomeContent.js
@@ -1171,7 +1171,7 @@ async function Talks({ confId }) {
         </CodeBlock>
       }
       right={
-        <NavContext.Provider value={{slug, navigate}}>
+        <NavContext value={{slug, navigate}}>
           <BrowserChrome
             domain="example.com"
             path={'confs/' + slug}
@@ -1191,7 +1191,7 @@ async function Talks({ confId }) {
               </Suspense>
             </ExamplePanel>
           </BrowserChrome>
-        </NavContext.Provider>
+        </NavContext>
       }
     />
   );

b09532b76f71afe9c3c5bf49d69b5883dd4ddd4c
Lauren Tan <poteto@users.noreply.github.com>
2025-09-16 19:00:26 -0400

    Add local eslint rule to validate markdown codeblocks with React Compiler
    In https://github.com/facebook/react/pull/34462 for example, we found an issue where the compiler was incorrectly validating an example straight from the docs.
    
    In order to find more issues like this + also provide more feedback to doc authors on valid/invalid patterns, this PR adds a new local eslint rule which validates all markdown codeblocks containing components/hooks with React Compiler. An autofixer is also provided.
    
    To express that a codeblock has an expected error, we can use the following metadata:
    
    ```ts
    // pseudo type def
    type MarkdownCodeBlockMetadata = {
        expectedErrors?: {
          'react-compiler'?: number[];
        };
      };
    ```
    
    and can be used like so:
    
    ````
    ```js {expectedErrors: {'react-compiler': [4]}}
    // ❌ setState directly in render
    function Component({value}) {
      const [count, setCount] = useState(0);
      setCount(value); // error on L4
      return <div>{count}</div>;
    }
    ```
    ````
    
    Because this is defined as a local rule, we don't have the same granular reporting that `eslint-plugin-react-hooks` yet. I can look into that later but for now this first PR just sets us up with something basic.

---
+++ eslint-local-rules/index.js
@@ -0,0 +1,34 @@
+const lintMarkdownCodeBlocks = require('./rules/lint-markdown-code-blocks');
+
+module.exports = {
+  rules: {
+    'lint-markdown-code-blocks': lintMarkdownCodeBlocks,
+  },
+  processors: {
+    markdown: {
+      preprocess(text, filename) {
+        return [
+          `// Markdown content for processing\n/* ${text.replace(/\*\//g, '*\\/')} */`
+        ];
+      },
+      postprocess(messages) {
+        const processedMessages = messages.flat().map(message => {
+          if (message.suggestions) {
+            const processedSuggestions = message.suggestions.map(suggestion => {
+              if (suggestion.fix) {
+                // Unescape */ in fix text
+                const fixedText = suggestion.fix.text.replace(/\*\\\//g, '*/');
+                return { ...suggestion, fix: { ...suggestion.fix, text: fixedText } };
+              }
+              return suggestion;
+            });
+            return { ...message, suggestions: processedSuggestions };
+          }
+          return message;
+        });
+        return processedMessages;
+      },
+      supportsAutofix: true
+    }
+  }
+};

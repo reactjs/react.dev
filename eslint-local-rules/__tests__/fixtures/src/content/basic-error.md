56be564bb64bf561049405e4a2cd18a5fe158887
Lauren Tan <poteto@users.noreply.github.com>
2025-09-17 11:48:56 -0400

    Add local eslint rule to validate markdown codeblocks with React Compiler
    In https://github.com/facebook/react/pull/34462 for example, we found an issue where the compiler was incorrectly validating an example straight from the docs.
    
    In order to find more issues like this + also provide more feedback to doc authors on valid/invalid patterns, this PR adds a new local eslint rule which validates all markdown codeblocks containing components/hooks with React Compiler. An autofixer is also provided.
    
    To express that a codeblock has an expected error, we can use the following metadata:
    
    ```ts
    // pseudo type def
    type MarkdownCodeBlockMetadata = {
        expectedErrors?: {
          'react-compiler'?: number[];
        };
      };
    ```
    
    and can be used like so:
    
    ````
    ```js {expectedErrors: {'react-compiler': [4]}}
    // ❌ setState directly in render
    function Component({value}) {
      const [count, setCount] = useState(0);
      setCount(value); // error on L4
      return <div>{count}</div>;
    }
    ```
    ````
    
    Because this is defined as a local rule, we don't have the same granular reporting that `eslint-plugin-react-hooks` yet. I can look into that later but for now this first PR just sets us up with something basic.

---
+++ eslint-local-rules/__tests__/fixtures/src/content/basic-error.md
@@ -0,0 +1,8 @@
+```jsx
+import {useState} from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+  setCount(count + 1);
+  return <div>{count}</div>;
+}
+```
---
+++ eslint-local-rules/index.js
@@ -0,0 +1,7 @@
+const lintMarkdownCodeBlocks = require('./rules/lint-markdown-code-blocks');
+
+module.exports = {
+  rules: {
+    'lint-markdown-code-blocks': lintMarkdownCodeBlocks,
+  },
+};

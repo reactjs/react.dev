c9c7a50b7afc60f84de453e88025e24855896b09
Lauren Tan <poteto@users.noreply.github.com>
2025-09-18 13:03:52 -0400

    Update copyright on all files
    Run the script.

--- plugins/remark-smartypants.js
+++ plugins/remark-smartypants.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 /*!
  * Based on 'silvenon/remark-smartypants'
  * https://github.com/silvenon/remark-smartypants/pull/80
